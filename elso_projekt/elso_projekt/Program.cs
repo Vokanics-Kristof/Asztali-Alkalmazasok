9f6593c0fa81b3789711f366f15542825773b52e
Vokánics Kristóf <vokanics.kristof@blathy.info>
2023-09-15 11:38:04 +0200

    Every program of today(09. 15)

---
+++ elso_projekt/elso_projekt/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace elso_projekt
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+                Console.BackgroundColor = ConsoleColor.Green;
+                Console.WriteLine("Hello world!");
+                Console.ReadKey();
+             */
+
+            Console.Title = "Program";
+
+            //Háttérszín beállítása + Console.Clear(); Kell!!
+            Console.BackgroundColor = ConsoleColor.Magenta;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.Clear();
+
+            Console.Write("Helló");
+            Console.WriteLine("Világ");
+
+            int terulet = 10;
+            int kerulet = 20;
+
+            Console.WriteLine("A kör kerülete: " + 10);
+            Console.WriteLine("A kör területe: " + 10);
+
+            Console.WriteLine($"A kör kerülete: {kerulet}, területe: {terulet}");
+            Console.WriteLine("A kör kerülete: {0}, kerülete: {1}", kerulet, terulet);
+
+            Console.ReadLine();
+
+            Console.Clear();
+
+            // Háttér és írószín visszaállítása
+            Console.ResetColor();
+            Console.Clear();
+
+            Console.ReadLine();
+
+            Console.SetCursorPosition(10, 20);
+            Console.WriteLine("valami");
+
+            Console.ReadLine();
+
+            Test t = new Test();
+            t.Loading();
+        }
+    }
+}
---
+++ elso_projekt/elso_projekt/Test.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace elso_projekt
+{
+    class Test
+    {
+        public static int second = 0;
+        public void Loading()
+        {
+            //Setting tha base colors
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.Black;
+
+            window w = new window();
+            w.DrawWindow();
+
+            Console.SetCursorPosition(Console.WindowWidth / 2 - 10, Console.WindowHeight / 2);
+
+            int i = 10;
+
+            while(i > -10)
+            {
+                if (second != DateTime.UtcNow.Second)
+                {
+                    Console.Write("a");
+                    Console.SetCursorPosition(Console.WindowWidth / 2 - i, Console.WindowHeight / 2);
+                    second = DateTime.UtcNow.Second;
+                    i--;
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+
+    class window
+    {
+        public string Title = "Window";
+        public int x = 20;
+        public int y = 10;
+
+        public void DrawWindow()
+        {
+            Console.SetCursorPosition(20, 10);
+            for(int i = 0; i < 50; i++)
+            {
+                Console.Write("+");
+            }
+            Console.SetCursorPosition(22, 10);
+            Console.Write(Title);
+            Console.SetCursorPosition(20, 10);
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write("+");
+                Console.SetCursorPosition(x + 50, y);
+                Console.Write("+");
+                y++;
+                Console.SetCursorPosition(x, y);
+            }
+            for (int i = 0; i < 51; i++)
+            {
+                Console.Write("+");
+            }
+        }
+    }
+}
