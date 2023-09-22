399283d7fb8749c8a34f7b99b8ab0804cb59c753
CrystalOSDevelopment <89468587+CrystalOSDevelopment@users.noreply.github.com>
2023-09-22 09:41:31 +0200

    Update(09. 22)

---
+++ elso_projekt/Feladatok/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Feladatok
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            elso();
+            masodik();
+            harmadik();
+        }
+
+        #region elso
+        static void elso()
+        {
+            Console.Write("A téglalap a oldala: ");
+            float a = float.Parse(Console.ReadLine());
+            Console.Write("A téglalap b oldala: ");
+            float b = float.Parse(Console.ReadLine());
+            Console.WriteLine($"A téglalap kerülete: {Ker(a, b)}, területe: {Ter(a, b)}");
+        }
+        static float Ker(float a, float b)
+        {
+            return (int)(a + b) * 2;
+        }
+        static float Ter(float a, float b)
+        {
+            return (int)(a * b);
+        }
+        #endregion elso
+
+        #region masodik
+        static void masodik()
+        {
+            Console.Write("Az egyenlet: ");
+            string num = Console.ReadLine();
+
+            num = num.Replace(" ", "");
+
+            if (num.Contains("+"))
+            {
+                string[] szamok = num.Split('+');
+
+                Console.WriteLine(int.Parse(szamok[0]) + int.Parse(szamok[1]));
+            }
+            else if (num.Contains("-"))
+            {
+                string[] szamok = num.Split('-');
+
+                Console.WriteLine(int.Parse(szamok[0]) - int.Parse(szamok[1]));
+            }
+            else if (num.Contains("*"))
+            {
+                string[] szamok = num.Split('*');
+
+                Console.WriteLine(int.Parse(szamok[0]) * int.Parse(szamok[1]));
+            }
+            else if (num.Contains("/"))
+            {
+                string[] szamok = num.Split('/');
+
+                Console.WriteLine(int.Parse(szamok[0]) / int.Parse(szamok[1]));
+            }
+        }
+        #endregion masodik
+
+        #region harmadik
+        static void harmadik()
+        {
+            Console.Write("Celsius: ");
+            float num = float.Parse(Console.ReadLine());
+            Console.WriteLine($"Ugyanez Fahrenheitben: {Fahrenheit(num)}");
+        }
+        static float Fahrenheit(float cels)
+        {
+            return cels * 9 / 5 + 32;
+        }
+        #endregion harmadik
+
+        #region negyedik
+        static void negyedik()
+        {
+
+        }
+        static float Circe_T(float rad)
+        {
+            return 0;
+        }
+        #endregion negyedik
+    }
+}
